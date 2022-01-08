cd33c6f7ec53f13d328d1c628922f31ee427c64c
Michelle-Wu03 <73873509+Michelle-Wu03@users.noreply.github.com>
2022-01-08 20:59:33 +0800

    Update store.php
    add function searchSales

--- store.php
+++ store.php
@@ -5,10 +5,12 @@
 <html>
   <head>
     <meta charset = "utf-8">
-    <title>Store</title>
+    <title>Store</title>    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src = "sales.js"></script>
   </head>
   <body>
     <!--navBar 頁首-->
@@ -27,19 +29,27 @@
           <form class="form-group col-sm-4" action="store_delete.php" method="post">
             <input type="text" class="form-control" id="storeName" placeholder="輸入分店名稱" name="storeName">
             <button type="submit" class="btn btn-primary">刪除</button>
+          </form>
+          <form class="form-group col-sm-4" action="store_searchSales.php" method="post">
+          <input type="text" class="form-control" id="searchStoreSales" placeholder="輸入分店名稱" name="searchStoreName">
+          <button type="button" id ="searchSales" class="btn btn-primary">查詢總銷量</button>
         </form>
-        <div class="col-sm-4 float-right">
-          <input type ="button" class="btn btn-primary" value="新增點我" onclick="location.href='add.php'"></input>
-          <input type ="button" class="btn btn-primary float-right" value="修改點我" onclick="location.href='update.php'"></input>
+          
         </div>
       </div>
-    </div>
+      <br>
+      <div class="col-sm-4">
+            <input type ="button" class="btn btn-primary" value="新增點我" onclick="location.href='add.php'"></input>
+            <input type ="button" class="btn btn-primary " value="修改點我" onclick="location.href='update.php'"></input>
+          </div>
       <br>
       <!--data-->
-	<?php
-		include "store_inform.php";
-	?>
-      
+      <div id="datas">
+      <?php
+          include "store_inform.php";
+        ?>
+      </div>
+        
     </div>
   </body>
 </html>
